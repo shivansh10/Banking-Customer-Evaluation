5cf34f80f126b48f9abe4f439bb9d559f1eacf37
Shivansh Bansal <shiv_vansh@Shivanshs-MacBook-Air.local>
2020-08-19 11:01:02 +0530

    initial commit

---
+++ ann.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+print(X)
+# One Hot Encoding the "Geography" column
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+X = X[:, 1:]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+ann = tf.keras.models.Sequential()
+
+#add input layer and first hdden layer
+ann.add(tf.keras.layers.Dense(units=6, activation = 'relu'))
+
+#add second hidden layer
+ann.add(tf.keras.layers.Dense(6, activation = 'relu'))
+
+#output layer
+ann.add(tf.keras.layers.Dense(1, activation = 'sigmoid'))
+
+#compiler
+ann.compile(optimizer= 'adam', loss= 'binary_crossentropy', metrics=['accuracy'])
+
+#fit ANN to the training set
+
+ann.fit(X_train, y_train, batch_size=32, epochs=100)
+
+print(ann.predict(sc.transform([[1, 0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])) > 0.5)
+
+
+y_pred = ann.predict(X_test)
+y_pred = (y_pred > 0.5)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
